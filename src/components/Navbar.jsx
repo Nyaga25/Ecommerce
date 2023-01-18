4b1f46d9d1f934bc94f6354bf5a6899eb1c62fee
Nick <nicknyaga@icloud.com>
2023-01-18 16:57:06 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+      height: 60px;
+      background-color: blue;
+
+`
+const Wrapper = styled.div`
+padding: 10px 20px;
+display: flex;
+justify-content:space-between;
+`
+const left = styled.div`
+width: 33.3%`;
+const center = styled.div`
+width: 33.3%`;
+const right = styled.div`
+width: 33.3%`;
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Container>
+      <Wrapper>
+        <left>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim natus inventore quam dolore, impedit a odio facere illo ipsam explicabo quidem alias modi dolores nesciunt ratione mollitia nulla blanditiis aperiam.</left>
+        <center>center</center>
+        <right>right</right>
+      </Wrapper>
+    </Container>
   )
 }
 
